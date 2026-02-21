d720bce6d0eace19b5999bff188053bda0a83e33
ewood213 <ewoodrum498@outlook.com>
2026-02-21 13:14:32 -0600

    Update rotate piece api

--- src/kanoodle/game/pieces.py
+++ src/kanoodle/game/pieces.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
 from enum import Enum
 import numpy as np
 from typing import List
+
 # The mirrored cases are always one above the "normal" ones
 class Rotation(Enum):
     Zero = 0
@@ -16,26 +18,33 @@ class Piece:
     def __init__(self, layout: np.ndarray):
         self.layout = layout
 
-    def get_layout(self, rotation: Rotation) -> np.ndarray:
+    def rotate_piece(self, rotation: Rotation) -> Piece:
         # Handle unmirrored cases
         if rotation == Rotation.Zero:
-            return self.layout
+            return Piece(self.layout)
         elif rotation == Rotation.Ninety:
-            return self.layout[::-1].T
+            return Piece(self.layout[::-1].T)
         elif rotation == Rotation.OneEighty:
-            return self.layout[::-1, ::-1]
+            return Piece(self.layout[::-1, ::-1])
         elif rotation == Rotation.TwoSeventy:
-            return self.layout[:, ::-1].T
+            return Piece(self.layout[:, ::-1].T)
         else:
             # Handle the mirrored cases (we can assume they are always one below the unmirroed ones)
             normal_rotation = Rotation(rotation.value - 1)
-            return self.get_layout(normal_rotation)[:, ::-1]
+            return Piece(self.rotate_piece(normal_rotation).layout[:, ::-1])
 
-    def get_all_rotations(self) -> List[np.ndarray]:
+    def get_all_rotations(self) -> List[Piece]:
         ret = []
         for rotation in Rotation:
-            new_layout = self.get_layout(rotation)
-            if not any([np.array_equal(arr, new_layout) for arr in ret]):
-                ret.append(new_layout)
+            rotated_piece = self.rotate_piece(rotation)
+            if not any([piece == rotated_piece for piece in ret]):
+                ret.append(rotated_piece)
         return ret
+    
+    def __eq__(self, other: Piece) -> bool:
+        return np.array_equal(self.layout, other.layout)
+    
+    def __repr__(self):
+        return f"layout: {self.layout}"
+
 
--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -2,116 +2,116 @@ import kanoodle.game.pieces as pieces
 import numpy as np
 
 def test_rotations_j_piece():
-    layout = np.array([[1, 1],
+    original = pieces.Piece(np.array([[1, 1],
                        [1, 0],
-                       [1, 0]])
+                       [1, 0]]))
 
-    ninety = np.array([[1, 1, 1],
-                       [0, 0, 1]])
+    ninety = pieces.Piece(np.array([[1, 1, 1],
+                       [0, 0, 1]]))
 
-    one_eighty = np.array([[0, 1],
+    one_eighty = pieces.Piece(np.array([[0, 1],
                            [0, 1],
-                           [1, 1]])
+                           [1, 1]]))
 
-    two_seventy = np.array([[1, 0, 0],
-                            [1, 1, 1]])
+    two_seventy = pieces.Piece(np.array([[1, 0, 0],
+                            [1, 1, 1]]))
 
-    layout_mirrored = np.array([[1, 1],
+    original_mirrored = pieces.Piece(np.array([[1, 1],
                                 [0, 1],
-                                [0, 1]])
+                                [0, 1]]))
 
-    ninety_mirrored = np.array([[1, 1, 1],
-                                [1, 0, 0]])
+    ninety_mirrored = pieces.Piece(np.array([[1, 1, 1],
+                                [1, 0, 0]]))
 
-    one_eighty_mirrored = np.array([[1, 0],
+    one_eighty_mirrored = pieces.Piece(np.array([[1, 0],
                                     [1, 0],
-                                    [1, 1]])
+                                    [1, 1]]))
 
-    two_seventy_mirrored = np.array([[0, 0, 1],
-                                     [1, 1, 1]])
+    two_seventy_mirrored = pieces.Piece(np.array([[0, 0, 1],
+                                     [1, 1, 1]]))
 
-    piece = pieces.Piece(layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.Zero), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.Ninety), ninety)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.OneEighty), one_eighty)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.TwoSeventy), two_seventy)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.ZeroMirroed), layout_mirrored)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.NinetyMirrored), ninety_mirrored)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.OneEightyMirrored), one_eighty_mirrored)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.TwoSeventyMirrored), two_seventy_mirrored)
+    piece = original
+    assert piece.rotate_piece(pieces.Rotation.Zero) ==  original
+    assert piece.rotate_piece(pieces.Rotation.Ninety) ==  ninety
+    assert piece.rotate_piece(pieces.Rotation.OneEighty) ==  one_eighty
+    assert piece.rotate_piece(pieces.Rotation.TwoSeventy) ==  two_seventy
+    assert piece.rotate_piece(pieces.Rotation.ZeroMirroed) ==  original_mirrored
+    assert piece.rotate_piece(pieces.Rotation.NinetyMirrored) ==  ninety_mirrored
+    assert piece.rotate_piece(pieces.Rotation.OneEightyMirrored) ==  one_eighty_mirrored
+    assert piece.rotate_piece(pieces.Rotation.TwoSeventyMirrored) ==  two_seventy_mirrored
 
-    all_rotations_expected = [layout, layout_mirrored, ninety, ninety_mirrored, one_eighty, one_eighty_mirrored, two_seventy, two_seventy_mirrored]
+    all_rotations_expected = [original, original_mirrored, ninety, ninety_mirrored, one_eighty, one_eighty_mirrored, two_seventy, two_seventy_mirrored]
     all_rotations_actual = piece.get_all_rotations()
 
     assert len(all_rotations_expected) == len(all_rotations_actual)
     for r_expected, r_actual in zip(all_rotations_expected, all_rotations_actual):
-        assert np.array_equal(r_expected, r_actual)
+        assert r_expected == r_actual
 
 def test_rotations_plus_piece():
-    layout = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
-    piece = pieces.Piece(layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.Zero), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.Ninety), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.OneEighty), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.TwoSeventy), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.ZeroMirroed), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.NinetyMirrored), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.OneEightyMirrored), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.TwoSeventyMirrored), layout)
+    original = pieces.Piece(np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]]))
+    piece = original
+    assert piece.rotate_piece(pieces.Rotation.Zero) == original
+    assert piece.rotate_piece(pieces.Rotation.Ninety) == original
+    assert piece.rotate_piece(pieces.Rotation.OneEighty) == original
+    assert piece.rotate_piece(pieces.Rotation.TwoSeventy) == original
+    assert piece.rotate_piece(pieces.Rotation.ZeroMirroed) == original
+    assert piece.rotate_piece(pieces.Rotation.NinetyMirrored) == original
+    assert piece.rotate_piece(pieces.Rotation.OneEightyMirrored) == original
+    assert piece.rotate_piece(pieces.Rotation.TwoSeventyMirrored) == original
 
-    all_rotations_expected = [layout]
+    all_rotations_expected = [original]
     all_rotations_actual = piece.get_all_rotations()
     assert len(all_rotations_expected) == len(all_rotations_actual)
     for r_expected, r_actual in zip(all_rotations_expected, all_rotations_actual):
-        assert(np.array_equal(r_expected, r_actual))
+        assert r_expected == r_actual
 
 def test_rotations_zigzag_piece():
-    layout = np.array([[1, 0, 0],
+    original = pieces.Piece(np.array([[1, 0, 0],
                        [1, 1, 0],
-                       [0, 1, 1]])
+                       [0, 1, 1]]))
 
-    ninety = np.array([[0, 1, 1],
+    ninety = pieces.Piece(np.array([[0, 1, 1],
                        [1, 1, 0],
-                       [1, 0, 0]])
+                       [1, 0, 0]]))
 
-    one_eighty = np.array([[1, 1, 0],
+    one_eighty = pieces.Piece(np.array([[1, 1, 0],
                            [0, 1, 1],
-                           [0, 0, 1]])
+                           [0, 0, 1]]))
 
-    two_seventy = np.array([[0, 0, 1],
+    two_seventy = pieces.Piece(np.array([[0, 0, 1],
                             [0, 1, 1],
-                            [1, 1, 0]])
+                            [1, 1, 0]]))
 
-    layout_mirrored = np.array([[0, 0, 1],
+    original_mirrored = pieces.Piece(np.array([[0, 0, 1],
                                 [0, 1, 1],
-                                [1, 1, 0]])
+                                [1, 1, 0]]))
 
-    ninety_mirrored = np.array([[1, 1, 0],
+    ninety_mirrored = pieces.Piece(np.array([[1, 1, 0],
                                 [0, 1, 1],
-                                [0, 0, 1]])
+                                [0, 0, 1]]))
 
-    one_eighty_mirrored = np.array([[0, 1, 1],
+    one_eighty_mirrored = pieces.Piece(np.array([[0, 1, 1],
                                     [1, 1, 0],
-                                    [1, 0, 0]])
+                                    [1, 0, 0]]))
 
-    two_seventy_mirrored = np.array([[1, 0, 0],
+    two_seventy_mirrored = pieces.Piece(np.array([[1, 0, 0],
                                      [1, 1, 0],
-                                     [0, 1, 1]])
+                                     [0, 1, 1]]))
 
-    piece = pieces.Piece(layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.Zero), layout)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.Ninety), ninety)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.OneEighty), one_eighty)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.TwoSeventy), two_seventy)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.ZeroMirroed), layout_mirrored)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.NinetyMirrored), ninety_mirrored)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.OneEightyMirrored), one_eighty_mirrored)
-    assert np.array_equal(piece.get_layout(pieces.Rotation.TwoSeventyMirrored), two_seventy_mirrored)
+    piece = original
+    assert piece.rotate_piece(pieces.Rotation.Zero) == original
+    assert piece.rotate_piece(pieces.Rotation.Ninety) == ninety
+    assert piece.rotate_piece(pieces.Rotation.OneEighty) == one_eighty
+    assert piece.rotate_piece(pieces.Rotation.TwoSeventy) == two_seventy
+    assert piece.rotate_piece(pieces.Rotation.ZeroMirroed) == original_mirrored
+    assert piece.rotate_piece(pieces.Rotation.NinetyMirrored) == ninety_mirrored
+    assert piece.rotate_piece(pieces.Rotation.OneEightyMirrored) == one_eighty_mirrored
+    assert piece.rotate_piece(pieces.Rotation.TwoSeventyMirrored) == two_seventy_mirrored
 
-    all_rotations_expected = [layout, layout_mirrored, ninety, ninety_mirrored]
+    all_rotations_expected = [original, original_mirrored, ninety, ninety_mirrored]
     all_rotations_actual = piece.get_all_rotations()
 
     assert len(all_rotations_expected) == len(all_rotations_actual)
     for r_expected, r_actual in zip(all_rotations_expected, all_rotations_actual):
-        assert np.array_equal(r_expected, r_actual)
+        assert r_expected == r_actual
 
