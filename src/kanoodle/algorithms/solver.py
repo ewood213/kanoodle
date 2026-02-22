1345b52a68a4fa44d7e0ddac3e5210dcd85bd1f8
ewood213 <ewoodrum498@outlook.com>
2026-02-21 19:36:40 -0600

    Add solver

---
+++ src/kanoodle/algorithms/solver.py
@@ -0,0 +1,106 @@
+import kanoodle.game.board as board
+import kanoodle.game.pieces as pieces
+import kanoodle.algorithms.algorithm_x as algx
+from typing import List
+import numpy as np
+import copy
+
+class PiecePlacement:
+    def __init__(self, row, col, layout):
+        self.row = row
+        self.col = col
+        self.layout = layout
+
+    def __repr__(self):
+        return f"Piece Placement: location: ({self.row}, {self.col}) layout: {self.layout}"
+
+class Solver:
+    def __init__(self, board: board.Board, pieces: List[pieces.Piece]):
+        self.board = board
+        self.pieces = pieces
+        num_zeros = np.count_nonzero(self.board.cells == 0)
+        num_filled_by_pieces = sum([np.count_nonzero(p.layout) for p in pieces])
+        assert num_zeros == num_filled_by_pieces
+
+    def create_incidence_matrix(self) -> np.ndarray:
+        incidence_matrix = []
+        rows = self.board.cells.shape[0]
+        cols = self.board.cells.shape[1]
+
+        # Our columns denote which piece, and which cell (i, j) the piece occupies
+        row_len = rows * cols + len(self.pieces)
+        for i, piece in enumerate(self.pieces):
+            for p in piece.get_all_rotations():
+                for r in range(rows):
+                    for c in range(cols):
+                        if self.board.can_place_piece(r, c, p):
+                            row = np.zeros(row_len, dtype=np.uint8)
+                            row[i] = 1
+                            places_filled = np.argwhere(p.layout) + np.array([r, c])
+                            for place in places_filled:
+                                row_loc = len(self.pieces) + place[0] * cols + place[1]
+                                row[row_loc] = 1
+                            incidence_matrix.append(row)
+
+        # Add a row that includes all spots used without a piece (must be in solution)
+        already_filled = np.argwhere(self.board.cells)
+        already_filled_row = np.zeros(row_len, dtype=np.uint8)
+        for c in already_filled:
+            row_loc = len(self.pieces) + c[0] * cols + c[1]
+            already_filled_row[row_loc] = 1
+        incidence_matrix.append(already_filled_row)
+
+        return np.array(incidence_matrix, dtype=np.uint8)
+
+    def valid_placement(self, row, column, piece):
+        if not self.board.can_place_piece(row, column, piece):
+            return False
+        placed_board = copy.deepcopy(self.board)
+        placed_board.place_piece(row, column, piece)
+        return not self.contains_small_holes(placed_board)
+
+    def contains_small_holes(self, board):
+        unfilled_cells = np.argwhere(board.cells==0)
+        min_hole_size_allowed = min((np.count_nonzero(p.layout) for p in self.pieces))
+        found = set()
+        for cell in unfilled_cells:
+            if tuple(cell) in found:
+                return
+            stack = [cell]
+            found.add(tuple)
+            count = 1
+            while stack:
+                cur = stack.pop()
+                for dir in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+                    next_loc = np.array([cur[0].astype(int) + dir[0], cur[1].astype(int) + dir[1]])
+                    gt_zero = np.all(next_loc >= 0)
+                    row_in_bounds = next_loc[0] < board.cells.shape[0]
+                    col_in_bounds = next_loc[1] < board.cells.shape[1]
+                    if gt_zero and row_in_bounds and col_in_bounds and board.cells[next_loc[0], next_loc[1]] == 0 and not tuple(next_loc) in found:
+                        stack.append(next_loc)
+                        found.add(tuple(next_loc))
+                        count += 1
+            if count < min_hole_size_allowed:
+                return True
+        return False
+
+
+    def solve(self) -> List[PiecePlacement]:
+        rows = self.board.cells.shape[0]
+        cols = self.board.cells.shape[1]
+        incidence_matrix = self.create_incidence_matrix()
+        rows_used = algx.exact_cover(self.create_incidence_matrix())
+
+        # Return the pieces used and their orientation
+        pieces_and_locations = np.array([incidence_matrix[r] for r in rows_used if np.any(incidence_matrix[r][:len(self.pieces)])])
+        ret = [None for _ in range(len(self.pieces))]
+        for p in pieces_and_locations:
+            p_index = np.argwhere(p[:len(self.pieces)]).reshape(1)
+            board_loc = p[len(self.pieces):].reshape(rows, cols)
+            p_location = np.argwhere(board_loc)
+            top_left_location = np.array([np.min(p_location[:, 0]), np.min(p_location[:, 1])])
+            bottom_right_location = np.array([np.max(p_location[:, 0]), np.max(p_location[:, 1])])
+            layout = board_loc[top_left_location[0]:bottom_right_location[0]+1, top_left_location[1]:bottom_right_location[1]+1]
+            ret[p_index[0]] = PiecePlacement(top_left_location[0], top_left_location[1], layout)
+        return ret
+
---
+++ tests/test_solver.py
@@ -0,0 +1,56 @@
+import kanoodle.game.pieces as pieces
+from kanoodle.game.board import Board
+from kanoodle.algorithms.solver import Solver
+import numpy as np
+
+def test_4x4_puzzle():
+    fat_piece = pieces.Piece(np.array([[1, 1], [1, 1], [1, 0]]))
+    squiggle_piece = pieces.Piece(np.array([[0, 1, 1], [1, 1, 0]]))
+    square_piece = pieces.Piece(np.ones((2, 2)))
+    bracket_piece = pieces.Piece(np.array([[0, 1], [1, 1]]))
+
+    board = Board(4, 4)
+    board.place_piece(0, 0, fat_piece)
+
+    solver = Solver(board, [squiggle_piece, square_piece, bracket_piece])
+    solution = solver.solve()
+    assert len(solution) == 3
+    squiggle_solution, square_solution, bracket_solution = solution
+    assert np.array_equal(squiggle_solution.layout, squiggle_piece.layout)
+    assert (squiggle_solution.row, squiggle_solution.col) == (2, 0)
+    assert np.array_equal(square_solution.layout, square_piece.layout)
+    assert (square_solution.row, square_solution.col) == (0, 2)
+    assert np.array_equal(bracket_solution.layout, bracket_piece.layout)
+    assert (bracket_solution.row, bracket_solution.col) == (2, 2)
+
+    for placement in solution:
+        piece = pieces.Piece(placement.layout)
+        board.place_piece(placement.row, placement.col, piece)
+    assert np.count_nonzero(board.cells) == board.cells.size
+
+def test_kanoodle_puzzle():
+    p1 = pieces.Piece(np.array([[1, 1], [0, 1]]))
+    p2 = pieces.Piece(np.array([[1, 0, 0], [1, 0, 0], [1, 1, 1]]))
+    p3 = pieces.Piece(np.array([[1, 1, 1, 1]]))
+    p4 = pieces.Piece(np.array([[1, 1, 1, 1], [1, 0, 0, 0]]))
+    p5 = pieces.Piece(np.array([[0, 0, 1, 1], [1, 1, 1, 0]]))
+    p6 = pieces.Piece(np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]]))
+    p7 = pieces.Piece(np.array([[1, 1], [1, 1]]))
+    p8 = pieces.Piece(np.array([[0, 1, 0, 0], [1, 1, 1, 1]]))
+    p9 = pieces.Piece(np.array([[1, 1, 0], [0, 1, 1], [0, 0, 1]]))
+    p10 = pieces.Piece(np.array([[1, 0, 1], [1, 1, 1]]))
+    p11 = pieces.Piece(np.array([[1, 1, 1], [0, 1, 1]]))
+    p12 = pieces.Piece(np.array([[0, 1], [0, 1], [1, 1]]))
+
+    board = Board(5, 11)
+    board.place_piece(0, 0, p1)
+    board.place_piece(1, 0, p2)
+    board.place_piece(4, 0, p3)
+    board.place_piece(0, 2, p4)
+    solver = Solver(board, [p5, p6, p7, p8, p9, p10, p11, p12])
+    solution = solver.solve()
+
+    for placement in solution:
+        piece = pieces.Piece(placement.layout)
+        board.place_piece(placement.row, placement.col, piece)
+    assert np.count_nonzero(board.cells) == board.cells.size
