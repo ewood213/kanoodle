f4c0466a23eba359e593b94fdaba88a25b22dc40
ewood213 <ewoodrum498@outlook.com>
2026-02-22 12:14:23 -0600

    Add dancing links solver

--- src/kanoodle/algorithms/dancing_links.py
+++ src/kanoodle/algorithms/dancing_links.py
@@ -1,21 +1,21 @@
 import numpy as np
+
 class Node:
-    def __init__(self, left, right, up, down, column, first_obj, name):
+    def __init__(self, left, right, up, down, column, name):
         self.left = left
         self.right = right
         self.up = up
         self.down = down
         self.column = column
-        self.first_obj = first_obj
         self.name = name
 
 class Column:
-    def __init__(self, left, right, up, down, first_obj, size, name):
+    def __init__(self, left, right, up, down, column, size, name):
         self.left = left
         self.right = right
         self.up = up
         self.down = down
-        self.first_obj = first_obj
+        self.column = column
         self.size = size
         self.name = name
 
@@ -25,9 +25,10 @@ def add_column_headers(root, incidence_matrix):
     cur_node = root
     for i in range(num_cols):
         size = np.count_nonzero(incidence_matrix[:, i])
-        new_column = Column(cur_node, None, None, None, None, size, f"{i+1}")
+        new_column = Column(cur_node, None, None, None, None, size, i)
         new_column.up = new_column
         new_column.down = new_column
+        new_column.column = new_column
         cur_node.right = new_column
         cur_node = new_column
     cur_node.right = root
@@ -35,7 +36,7 @@ def add_column_headers(root, incidence_matrix):
 
 
 def create_links_from_mat(incidence_matrix):
-    root = Column(None, None, None, None, None, None, "0")
+    root = Column(None, None, None, None, None, None, -1)
     add_column_headers(root, incidence_matrix)
     for i, row in enumerate(incidence_matrix):
         one_locations = np.argwhere(row).reshape(-1)
@@ -53,18 +54,11 @@ def create_links_from_mat(incidence_matrix):
                 cur_col = cur_col.right
 
             # Create our node, and set its left, up and down parameters
-            new_node = Node(last_node, None, cur_col.up, cur_col, cur_col, None, [i, one_location])
+            new_node = Node(last_node, None, cur_col.up, cur_col, cur_col, [i, one_location])
 
             # Insert node into up down list
             cur_col.up.down = new_node
             cur_col.up = new_node
-
-            # Set the first obj
-            if cur_col.first_obj is None:
-                cur_col.first_obj = new_node
-                new_node.first_obj = new_node
-            else:
-                new_node.first_obj = cur_col.first_obj
 
             # Set right node
             if last_node is not None:
@@ -83,4 +77,109 @@ def create_links_from_mat(incidence_matrix):
     # Phew!
     return root
 
+def get_smallest_col(root):
+    cur = root.right
+    ret = cur
+    smallest_size = cur.size
+    while cur is not root:
+        if cur.size < smallest_size:
+            ret = cur
+            smallest_size = cur.size
+        cur = cur.right
+    return ret
+
+def cover_column(c):
+    # Remove column
+    c.right.left = c.left
+    c.left.right = c.right
+
+    # Remove column elements that share ones
+    i = c.down
+    while i is not c:
+        j = i.right
+        while j is not i:
+            j.down.up = j.up
+            j.up.down = j.down
+            j.column.size -= 1
+            j = j.right
+        i = i.down
+
+def uncover_column(c):
+    # Restore column elements that share ones
+    i = c.up
+    while i is not c:
+        j = i.left
+        while j is not i:
+            j.column.size += 1
+            j.down.up = j
+            j.up.down = j
+            j = j.left
+        i = i.up
+
+    # Restore column
+    c.right.left = c
+    c.left.right = c
+
+def _search(root, cur_sol, k):
+    if root.right == root:
+        return cur_sol
+    c = get_smallest_col(root)
+    if c.size == 0:
+        return None
+
+    # Remove column and add first row in column as solution
+    cover_column(c)
+    r = c.down
+    if len(cur_sol) < k+1:
+        assert len(cur_sol) == k
+        cur_sol.append(None)
+    while r is not c:
+        cur_sol[k] = r
+        j = r.right
+
+        # Remove columns that are also contained in row
+        while j is not r:
+            cover_column(j.column)
+            j = j.right
+
+        # Recurse to find solution
+        ret = _search(root, cur_sol, k+1)
+        if ret is not None:
+            return ret
+
+        # Now begin restoring...
+        c = r.column
+        j = r.left
+        while j is not r:
+            uncover_column(j.column)
+            j = j.left
+        r = r.down
+    uncover_column(c)
+
+
+def search(root):
+    solution = _search(root, [], 0)
+    if solution is None:
+        return None
+    ret = []
+    for r in [s for s in solution if s is not None]:
+        row = []
+        row.append(r.column.name)
+        j = r.right
+        while j is not r:
+            row.append(j.column.name)
+            j = j.right
+        ret.append(row)
+    return ret
+
+def solve_using_dancing_links(incidence_matrix):
+    root = create_links_from_mat(incidence_matrix)
+    solution = search(root)
+    if solution is None:
+        return None
+    ret = np.zeros((len(solution), incidence_matrix.shape[1]), dtype=np.uint8)
+    for i in range(len(solution)):
+        for j in solution[i]:
+            ret[i][j] = 1
+    return ret
 
--- tests/test_dancing_links.py
+++ tests/test_dancing_links.py
@@ -14,7 +14,7 @@ def test_header_creation():
     start = root.right
     i = 0
     while start is not root:
-        assert start.name == str(i+1)
+        assert start.name == i
         assert start.size == column_size[i]
         assert start.up == start.down == start
         start = start.right
@@ -24,7 +24,7 @@ def test_header_creation():
 
     i = 6
     while start is not root:
-        assert start.name == str(i+1)
+        assert start.name == i
         assert start.size == column_size[i]
         start = start.left
         i-=1
@@ -35,46 +35,46 @@ def test_create_links():
     root = dl.create_links_from_mat(test_matrix)
 
     # test column headers
-    assert root.right.name == '1'
+    assert root.right.name == 0
     assert root.right.size == 2
-    assert root.right.right.name == '2'
+    assert root.right.right.name == 1
     assert root.right.right.size == 3
-    assert root.right.right.right.name == '3'
+    assert root.right.right.right.name == 2
     assert root.right.right.right.size == 0
     assert root.right.right.right.right == root
-    assert root.left.name == '3'
-    assert root.left.left.name == '2'
-    assert root.left.left.left.name == '1'
+    assert root.left.name == 2
+    assert root.left.left.name == 1
+    assert root.left.left.left.name == 0
     assert root.left.left.left.left == root
 
     # test first col
     first_col_start = root.right.down
     assert first_col_start.name == [1, 0]
-    assert first_col_start.first_obj == first_col_start
+    assert first_col_start.column == root.right
     assert first_col_start.down.name == [2, 0]
-    assert first_col_start.down.down.name == '1'
-    assert first_col_start.up.name == '1'
-    assert first_col_start.down.first_obj == first_col_start
+    assert first_col_start.down.down.name == 0
+    assert first_col_start.up.name == 0
+    assert first_col_start.down.column == root.right
 
     # test second col
     second_col_start = root.right.right.down
     assert second_col_start.name == [0, 1]
-    assert second_col_start.first_obj == second_col_start
+    assert second_col_start.column == root.right.right
     assert second_col_start.down.name == [1, 1]
-    assert second_col_start.down.first_obj == second_col_start
+    assert second_col_start.down.column == root.right.right
     assert second_col_start.down.down.name == [2, 1]
-    assert second_col_start.down.down.first_obj == second_col_start
-    assert second_col_start.down.down.down.name == "2"
-    assert second_col_start.up.name == '2'
+    assert second_col_start.down.down.column == root.right.right
+    assert second_col_start.down.down.down.name == 1
+    assert second_col_start.up.name == 1
     assert second_col_start.up.up.name == [2, 1]
     assert second_col_start.up.up.up.name == [1, 1]
     assert second_col_start.up.up.up.up == second_col_start
 
     # test third column
     third_column_start = root.left.up
-    assert third_column_start.name == '3'
+    assert third_column_start.name == 2
     assert third_column_start.up == third_column_start.down == third_column_start
-    assert third_column_start.first_obj == None
+    assert third_column_start.column == third_column_start
 
     # test first row
     first_row_start = second_col_start
@@ -94,3 +94,40 @@ def test_create_links():
     assert third_row_start.right.right == third_row_start
     assert third_row_start.left.name == [2, 1]
     assert third_row_start.left.left == third_row_start
+
+def test_solve_dancing_links():
+    test_matrix = np.array([[0, 0, 1, 0, 1, 1, 0],
+                                [1, 0, 0, 1, 0, 0, 1],
+                                [0, 1, 1, 0, 0, 1, 0],
+                                [1, 0, 0, 1, 0, 0, 0],
+                                [0, 1, 0, 0, 0, 0, 1],
+                                [0, 0, 0, 1, 1, 0, 1]])
+    expected_solution = np.zeros((3, 7), dtype=np.uint8)
+    expected_solution[0] = test_matrix[3]
+    expected_solution[1] = test_matrix[0]
+    expected_solution[2] = test_matrix[4]
+    assert np.array_equal(dl.solve_using_dancing_links(test_matrix), expected_solution)
+
+def test_wikipedia_solution():
+    array = np.array([[1, 0, 0, 1, 0, 0, 1],
+                      [1, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 1, 1, 0, 1],
+                      [0, 0, 1, 0, 1, 1, 0],
+                      [0, 1, 1, 0, 0, 1, 1],
+                      [0, 1, 0, 0, 0, 0, 1]])
+    expected_solution = np.zeros((3, 7), dtype=np.uint8)
+    expected_solution[0] = array[1]
+    expected_solution[1] = array[3]
+    expected_solution[2] = array[5]
+
+    assert np.array_equal(dl.solve_using_dancing_links(array), expected_solution)
+
+def test_no_solution():
+    array = np.array([[0, 0, 0, 1, 0, 0, 1],
+                      [0, 0, 0, 1, 0, 0, 0],
+                      [0, 0, 0, 1, 1, 0, 1],
+                      [0, 0, 1, 0, 1, 1, 0],
+                      [0, 1, 1, 0, 0, 1, 1],
+                      [0, 1, 0, 0, 0, 0, 1]])
+
+    assert dl.solve_using_dancing_links(array) == None
